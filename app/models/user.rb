313365b9926c8d4f67369d32f2ec044342df5e8f
gamagaerun01 <chip-ma3ua12@au.com>
2021-03-21 20:18:22 +0000

    Initial Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
    has_many :books, dependent: :destroy
    has_many :book_comments, dependent: :destroy
    has_many :favorites, dependent: :destroy
+   def already_favorited?(book)
+    self.favorites.exists?(book_id: book.id)
+   end
    attachment :profile_image
    validates :name, presence: true, uniqueness: true,
                     length: {minimum: 2, maximum: 20}
